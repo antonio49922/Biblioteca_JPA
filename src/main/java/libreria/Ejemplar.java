139f023d62b64b5b57be9be646728aac422a9008
antonio <antoniobarahona492@gmail.com>
2024-12-10 09:11:57 +0100

    lo basico

---
+++ src/main/java/libreria/Ejemplar.java
@@ -0,0 +1,64 @@
+package libreria;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "ejemplar")
+public class Ejemplar {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "isbn", nullable = false)
+    private libreria.Libro isbn;
+
+    @ColumnDefault("'Disponible'")
+    @Lob
+    @Column(name = "estado")
+    private String estado;
+
+    @OneToMany(mappedBy = "ejemplar")
+    private Set<libreria.Prestamo> prestamos = new LinkedHashSet<>();
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public libreria.Libro getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(libreria.Libro isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public Set<libreria.Prestamo> getPrestamos() {
+        return prestamos;
+    }
+
+    public void setPrestamos(Set<libreria.Prestamo> prestamos) {
+        this.prestamos = prestamos;
+    }
+
+}
